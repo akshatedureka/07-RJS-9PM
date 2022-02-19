34f31ead24701d3d42f76eb84e6e3e53111dceed
Sambasivarao <samba.pendela@gmail.com>
2022-02-19 21:36:47 +0530

    arrays

--- JS/arrays.js
+++ JS/arrays.js
@@ -34,21 +34,209 @@
 
 
 
-//push()
-//pop()
-//unshift()
-//shift()
-let arr = [20,30,40];
-console.log( arr.length );                  //3
-console.log( arr );                         //[ 20, 30, 40 ]
-arr.push(50);
-console.log( arr );                         //[ 20, 30, 40, 50 ]
-arr.unshift(10);
-console.log( arr );                         //[ 10, 20, 30, 40, 50 ]
-arr.pop();
-console.log( arr );                        //[ 10, 20, 30, 40 ] 
-arr.shift();
-console.log(arr);                           //[ 20, 30, 40 ]
+/*
+    //push()
+    //pop()
+    //unshift()
+    //shift()
+    let arr = [20,30,40];
+    console.log( arr.length );                  //3
+    console.log( arr );                         //[ 20, 30, 40 ]
+    arr.push(50);
+    console.log( arr );                         //[ 20, 30, 40, 50 ]
+    arr.unshift(10);
+    console.log( arr );                         //[ 10, 20, 30, 40, 50 ]
+    arr.pop();
+    console.log( arr );                        //[ 10, 20, 30, 40 ] 
+    arr.shift();
+    console.log(arr);                           //[ 20, 30, 40 ]
+*/
+
+
+/*
+    //delete
+    //deletes the element at particular index
+    //delete property never releses the memory of deleted element
+    let arr = [10,20,30,40,50];
+    console.log( arr.length );                      //5
+    delete arr[0];
+    console.log( arr.length );                     //5
+    console.log(arr);                               //[ <1 empty item>, 20, 30, 40, 50 ]
+    delete arr[4];
+    console.log( arr.length );                      //5
+    console.log( arr );                             //[ <1 empty item>, 20, 30, 40, <1 empty item> ]                  
+*/
+
+
+
+/*
+    //splice()
+    //it is used to add/remove the element at any index
+    let arr1 = [10,20,30,40,50,60,70,80,90,100];
+    arr1.splice(4,3);
+    console.log( arr1 );                //[10,20,30,70,80,90,100]
+
+
+    arr1.splice(6,1);
+    console.log( arr1 );                //[ 10, 20, 30, 40, 80, 90 ]
+
+
+    arr1.splice(0,1);
+    console.log( arr1 );                //[ 20, 30, 40, 80, 90 ]
+
+
+    arr1.splice(1,1);
+    console.log( arr1 );                //[ 20, 40, 80, 90 ]
+
+
+    arr1.splice(2,0,50,60,70);;
+    console.log(arr1);                  //[20, 40, 50, 60,  70, 80, 90]
+
+
+    arr1.splice(1,1,30,40);
+    console.log( arr1 );                //[ 20, 30, 40, 50, 60, 70, 80, 90 ] 
+
+
+    arr1.splice(0,0,10);
+    console.log( arr1 );                //[ 10, 20, 30, 40, 50, 60, 70, 80, 90 ]
+
+
+    arr1.splice(9,0,100);
+    console.log( arr1 );                //[ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 ]
+*/
+
+//findIndex()
+let arr = [10,20,30,40,50,60,70,80,90,100];
+
+console.log(
+    arr.findIndex((element,index)=>{
+        return element == 50;
+    })
+);          //4
+
+
+console.log(
+    arr.findIndex((element,index)=>{
+        return element == 80;
+    })
+);      //7
+
+
+console.log(
+    arr.findIndex((element,index)=>{
+        return element == 1000;
+    })
+);          //-1
+
+arr.splice(arr.findIndex((element,index)=>{
+    return element == 50;
+}),1);
+console.log( arr );
+
+
+arr.splice( arr.findIndex((element,index)=>{
+    return element == 90;
+}), 1);
+console.log( arr );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
