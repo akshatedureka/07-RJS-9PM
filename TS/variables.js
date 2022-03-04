c6a761fa51d2300d7f8cdfc8543afa36cce9dfa9
Sambasivarao <samba.pendela@gmail.com>
2022-03-04 21:51:19 +0530

    variables are completed

--- TS/variables.js
+++ TS/variables.js
@@ -45,3 +45,52 @@ var x;
 console.log(x); //undefined
 x = null;
 console.log(x); //null
+var var1 = "Hello";
+var1 = 1000;
+console.log(var1);
+//1000
+var var2 = "global data type";
+console.log(var2);
+//global data type
+var num_arr1 = [100, 200, 300, 400, 500];
+var num_arr2 = [600, 700, 800, 900, 1000];
+num_arr1.forEach(function (element, index) {
+    console.log(element, num_arr2[index]);
+});
+var str_arr1 = ["Hello_1", "Hello_2", "Hello_3", "Hello_4", "Hello_5"];
+var str_arr2 = ["Welcome_1", "Welcome_2", "Welcome_3", "Welcome_4", "Welcome_5"];
+str_arr1.forEach(function (element, index) {
+    console.log(element, str_arr2[index]);
+});
+var bool_arr1 = [true, false, true, false, true];
+var bool_arr2 = [false, true, false, true, false];
+bool_arr1.forEach(function (element, index) {
+    console.log(element, bool_arr2[index]);
+});
+//JSON
+//JSON Stands for "Java Script Object Notation"
+//JSON also called as JavaScript Objects
+//JSON, used to transfer the data over the "Network"
+//JSON is "light" weight
+//JSON default datatype is "any"
+//Objects  --- {}
+//Arrays   --- []
+//data     --- key & value pairs
+//key & value separated by using ":"
+var obj = {
+    "sub_one": "ReactJS",
+    "sub_two": "NodeJS",
+    "sub_three": "MongoDB"
+};
+console.log(obj.sub_one, obj.sub_two, obj.sub_three);
+//ReactJS NodeJS MongoDB
+var json_array = [
+    { "p_id": 111, "p_name": "p_one", "p_cost": 10000 },
+    { "p_id": 222, "p_name": "p_two", "p_cost": 20000 },
+    { "p_id": 333, "p_name": "p_three", "p_cost": 30000 },
+    { "p_id": 444, "p_name": "p_four", "p_cost": 40000 },
+    { "p_id": 555, "p_name": "p_five", "p_cost": 50000 }
+];
+json_array.forEach(function (element, index) {
+    console.log(element.p_id, element.p_name, element.p_cost, index);
+});
