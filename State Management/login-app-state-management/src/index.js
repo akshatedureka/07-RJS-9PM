bbff3782d59ca7051e67055a3de2bce562d065f3
Sambasivarao <samba.pendela@gmail.com>
2022-04-17 12:56:13 +0530

    code

--- State Management/login-app-state-management/src/index.js
+++ State Management/login-app-state-management/src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore,applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { loginReducer } from "./reducer/loginReducer";
+import thunk from "redux-thunk";
+
+const store = createStore(loginReducer,applyMiddleware(thunk));
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
