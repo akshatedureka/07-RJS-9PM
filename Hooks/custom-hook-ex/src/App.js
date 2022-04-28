1f56f1bf08dd45a09c46d3618274cecb54f87e6b
Sambasivarao <samba.pendela@gmail.com>
2022-04-28 21:45:17 +0530

    code

--- Hooks/custom-hook-ex/src/App.js
+++ Hooks/custom-hook-ex/src/App.js
@@ -1,19 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-import useCalc from "./hooks/useCalc";
+/*
+  import logo from './logo.svg';
+  import './App.css';
+  import useCalc from "./hooks/useCalc";
 
-function App() {
-  
-  const [count,increment,decrement,reset] = useCalc(0,10);
-  
-  return (
-    <div className="App">
-      <h3>Count....{count}</h3>
-      <button onClick={increment}>Inc</button>
-      <button onClick={decrement}>Decr</button>
-      <button onClick={reset}>Reset</button>
-    </div>
-  );
+  function App() {
+    
+    const [count,increment,decrement,reset] = useCalc(0,10);
+    
+    return (
+      <div className="App">
+        <h3>Count....{count}</h3>
+        <button onClick={increment}>Inc</button>
+        <button onClick={decrement}>Decr</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    );
+  }
+
+  export default App;
+
+*/
+
+import React, { useState } from "react";
+import useCounter from "./hooks/useCounter";
+
+function App(){
+    const [count,setCount] = useState(0);
+    useCounter(count);
+
+    return(
+      <React.Fragment>
+          <button onClick={()=>setCount(count+1)}>Count - {count}</button>
+      </React.Fragment>
+    )
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
